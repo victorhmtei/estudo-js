745a423da8fd6b57623a967867fef5ab0343996f
Victor Teixeira <vhmteixeira@gmail.com>
2021-10-19 15:50:13 -0300

    desafios sobre  tipos de dados

---
+++ Desafios/desafio_area.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+
+
+
+let raio = 10
+const PI = 3.14
+
+let areaCircunferencia = (PI * raio * raio)
+
+console.log("A área da circunferencia é: " + areaCircunferencia)
---
+++ Desafios/desafio_troca.js
@@ -0,0 +1,11 @@
+// trocar valor de duas variaveis
+
+var a = 7
+var b = 94
+let temp = a
+
+a = b
+b = temp
+
+
+console.log('a:', a, 'b:', b)
